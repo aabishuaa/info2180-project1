4b4ac897c5194ac561eb1e2f63165a89fd554e70
Abishua Johnson <abishuasjohnson@gmail.com>
2023-11-02 12:55:30 -0500

    All images and Javascript code added

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
-/* Add your JavaScript to this file */
+//Javascript Code for INFO2180-project1
+var starter = function () {
+    var btn = document.getElementsByClassName("btn")[1]; 
+    var entry = document.getElementById("email");
+    var message = document.getElementsByClassName("message")[0];
+
+    btn.addEventListener("click", function (e) {
+        e.preventDefault(); 
+
+        var input = entry.value;
+        var emailFormat = /\S+@\S+\.\S+/; 
+
+        if (input === "") {
+            message.textContent = "Please enter a valid email address";
+        } else if (!emailFormat.test(input)) {
+            message.textContent = "Please enter a valid email address in the format name@example.com";
+        } else {
+            message.textContent = "Thank you! Your email address " + input + " has been added to our mailing list";
+        }
+    });
+};
+
+window.onload = starter;
+
